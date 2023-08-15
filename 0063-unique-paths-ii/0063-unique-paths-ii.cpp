45ebfb1c091d12751e48285eeb601e795c88d700
Anish Ambekar <49511019+anishambekar1999@users.noreply.github.com>
2023-08-15 11:18:51 +0530

    Time: 0 ms (100%), Space: 7.6 MB (80.45%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -4,42 +4,49 @@ public:
         
         if(obstacleGrid[0][0]==1)
             return 0;
-            
+
         int n=obstacleGrid.size();
         int m=obstacleGrid[0].size();
-        vector<vector<int>>ans(n,vector<int>(m,0));
+        vector<vector<int>>ans(2,vector<int>(m,0));
 
-        for(int i=0;i<n;i++)
-        {
-            if(obstacleGrid[i][0]==1)
-                break;
-            ans[i][0]=1;
-        }
-
-        for(int j=1;j<m;j++)
+        for(int j=0;j<m;j++)
         {
             if(obstacleGrid[0][j]==1)
                 break;
             ans[0][j]=1;
         }
 
+        bool flag=false;
+
         for(int i=1;i<n;i++)
         {
-            for(int j=1;j<m;j++)
+            for(int j=0;j<m;j++)
             {
+                if(j==0)
+                {
+                    if(obstacleGrid[i][0]==1)
+                        flag=true;
+                    
+                    if(flag)
+                        ans[i%2][j]=0;
+                    else
+                        ans[i%2][j]=1;
+                    continue;
+                }
+
                 if(obstacleGrid[i][j]==1)
                     continue;
                 
                 if(obstacleGrid[i-1][j]==1 && obstacleGrid[i][j-1]==1)
-                    ans[i][j]=0;
+                    ans[i%2][j]=0;
                 else if(obstacleGrid[i-1][j]==1)
-                    ans[i][j]=ans[i][j-1];
+                    ans[i%2][j]=ans[i%2][j-1];
                 else if(obstacleGrid[i][j-1]==1)
-                    ans[i][j]=ans[i-1][j];
+                    ans[i%2][j]=ans[1-(i%2)][j];
                 else
-                    ans[i][j]=ans[i][j-1]+ans[i-1][j];
+                    ans[i%2][j]=ans[i%2][j-1]+ans[1-(i%2)][j];
             }
         }
-        return ans[n-1][m-1];
+        return ans[(n-1)%2][m-1];
     }
 };
