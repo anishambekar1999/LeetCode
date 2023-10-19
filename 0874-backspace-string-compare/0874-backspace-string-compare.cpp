f580b13c437492e8fd9602ae3a0fc903305d590e
Anish Ambekar <49511019+anishambekar1999@users.noreply.github.com>
2023-10-19 09:51:14 +0530

    Time: 0 ms (100%), Space: 6.9 MB (15.59%) - LeetHub

---
+++ 0874-backspace-string-compare/0874-backspace-string-compare.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    string getString(string s, int n)
+    {
+        int idx = n-1;
+        string str = "";
+        int skip = 0;
+
+        while(idx >= 0)
+        {
+            while(idx >=0 && s[idx] == '#')
+            {
+                skip++;
+                idx--;
+            }
+
+            while(skip > 0 && idx>=0)
+            {
+                if(s[idx]=='#')
+                    skip++;
+                else
+                    skip--;
+                idx--;
+            }
+
+            while(idx>=0 && s[idx]!='#')
+            {
+                str.push_back(s[idx]);
+                idx--;
+            }
+        }
+        reverse(str.begin(),str.end());
+        return str;
+    }
+
+    bool backspaceCompare(string s, string t) {
+        
+        int n1 = s.length();
+        int n2 = t.length();
+
+        string str1 = getString(s,n1);
+        string str2 = getString(t,n2);
+
+        cout<<str1<<"\n";
+        cout<<str2<<"\n";
+        
+        return (str1 == str2);
+
+    }
+};
