fb604743122897bb13731d439004762bbb914226
Anish Ambekar <49511019+anishambekar1999@users.noreply.github.com>
2024-06-07 20:45:41 +0530

    Time: 822 ms (9.3%), Space: 379.9 MB (16.27%) - LeetHub

---
+++ 0648-replace-words/0648-replace-words.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    string replaceWords(vector<string>& dictionary, string sentence) {
+        
+        int m = dictionary.size();
+        int n = sentence.size();
+        unordered_map<string ,int>MP;
+
+        for(int i=0;i<m;i++)
+        {
+            MP[dictionary[i]]++;
+        }
+
+        string ans = "";
+        string s="";
+
+        for(int i=0;i<n;i++)
+        {
+            if(sentence[i] == ' ')
+            {
+                int sz = s.size();
+                if(sz > 0)
+                {
+                    ans.append(s);
+                    ans.push_back(' ');
+
+                    s = "";
+                }
+            }
+            else
+            {
+                s.push_back(sentence[i]);
+                if(MP[s] > 0)
+                {
+                    ans.append(s);
+                    ans.push_back(' ');
+
+                    i++;
+                    while(i<n && sentence[i]!=' ')
+                        i++;
+                    
+                    s="";
+                }
+            }
+        }
+
+        int sz = s.size();
+        if(sz > 0)
+        {
+            ans.append(s);
+            ans.push_back(' ');
+
+            s = "";
+        }
+
+        ans.pop_back();
+        return ans;
+    }
+};
